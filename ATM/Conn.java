e90ef250674f1dd9edaee79eefb058a5014c261f
Saurabh-debugs <65465836+Saurabh-debugs@users.noreply.github.com>
2020-12-01 10:35:32 +0530

    Add files via upload

---
+++ ATM/Conn.java
@@ -0,0 +1,21 @@
+package ATM;
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bank_database","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+
+
